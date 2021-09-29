c6b39a672f715024549c6fd6efde8cf061eb3065
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-09-28 19:19:04 -0700

    delete user button works correctly

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -1,19 +1,28 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useParams, useHistory } from 'react-router-dom';
 // import useLocalStorage from 'react-localstorage-hook';
 // import { updateUser } from '../utils/userUtils';
+import { deleteUser } from '../utils/userUtils';
 
 
 
 const ProfilePage = () => {
-
-  // const [updatedUser, setUser] = useState();
-
+  const { username } = useParams();
+  const [user, setUser] = useState();
+  const history = useHistory();
   // async function handleUpdateProfile(event) {
   //   event.preventDefault();
   //   const updateProfile = await updateUser();
   //   setUser(updateProfile);
   // }
+
+  async function handleDelete(event) {
+    event.preventDefault();
+    const userProfile = await deleteUser(username);
+    setUser({ ...user, userProfile });
+    history.push('/');
+  }
+
   return (
     <>
       <div>
@@ -26,7 +35,9 @@ const ProfilePage = () => {
         <button>Update Profile</button>
         {/* Logout uses window.location to return home 
     and set back to initial state */}
-        <button>Delete Account</button>
+        <button
+          onClick={handleDelete}
+        >Delete Account</button>
         <button>Logout</button>
         <Link to="/">
           <button>Home</button>
